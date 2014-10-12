c0e240d50d775fb06ea2c34b5fcb62ea9c328d93
neomulik <neomulik@hotmail.com>
2014-10-12 21:15:22 +0300

    first version

--- lesson-2/hw/index.php
+++ lesson-2/hw/index.php
@@ -1,2 +1,26 @@
 <?php
-    echo "work";
+    class calculator {
+
+        public static function doIt ($operandA, $operandB, $operator) {
+
+            switch ($operator) {
+                case '+':
+                    $result = $operandA + $operandB;
+                    break;
+
+                case '-':
+                    $result = $operandA - $operandB;
+                    break;
+                case '*':
+                    $result = $operandA * $operandB;
+                    break;
+                case '/':
+                    $result = $operandA / $operandB;
+                    break;
+            }
+            print($result);
+        }
+    }
+
+$result = calculator::doIt($argv[1],$argv[2],$argv[3]);
+print($result);
