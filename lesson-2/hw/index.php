45f2947e6e75a10198f94caf3bfb2f1afc4a9e93
neomulik <neomulik@hotmail.com>
2014-10-20 13:48:34 +0300

    add some attributes

--- lesson-2/hw/index.php
+++ lesson-2/hw/index.php
@@ -1,5 +1,10 @@
 <?php
+error_reporting(0);
+
     class calculator {
+        public static $info_1 = "Ошибка, программа работает только с этими операторами: +, -, *, /";
+        public static $info_2 = "Результат: Ошибка, 1 и 3 параметры должны быть числом\r\n";
+        public static $info_3 = "Результат: Ошибка, метод ожидает 3 параметра (Операнд, Оператор, Операнд)\r\n";
 
         public static function doIt ($operandA, $operator, $operandB) {
             if ($operandA !== null && $operandB !== null && $operator !== null) {
@@ -18,15 +23,15 @@
                             $result = $operandA / $operandB;
                             break;
                         default:
-                            $result = "Ошибка, программа работает только с этими операторами: +, -, *, /";
+                            $result = self::$info_1;
                             break;
                     }
                     $result = "Результат: " . $result . "\r\n";
                 } else {
-                    $result = "Результат: Ошибка, 1 и 3 параметры должны быть числом\r\n";
+                    $result = self::$info_2;
                 }
             } else {
-                $result = "Результат: Ошибка, метод ожидает 3 параметра\r\n";
+                $result = self::$info_3;
             }
             print($result);
         }
