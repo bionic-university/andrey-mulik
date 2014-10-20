79cfcaf8b1229666e684f7ceb504978be7297ebb
neomulik <neomulik@hotmail.com>
2014-10-20 13:36:27 +0300

    add some error handling

--- lesson-2/hw/index.php
+++ lesson-2/hw/index.php
@@ -1,26 +1,33 @@
 <?php
     class calculator {
 
-        public static function doIt ($operandA, $operandB, $operator) {
-
-            switch ($operator) {
-                case '+':
-                    $result = $operandA + $operandB;
-                    break;
-                case '-':
-                    $result = $operandA - $operandB;
-                    break;
-                case '*':
-                    $result = $operandA * $operandB;
-                    break;
-                case '/':
-                    $result = $operandA / $operandB;
-                    break;
-                default:
-                    $result = "Ошибка, программа работает только с этими операторами: +, -, *, /";
-                    break;
+        public static function doIt ($operandA, $operator, $operandB) {
+            if ($operandA !== null && $operandB !== null && $operator !== null) {
+                if (is_numeric($operandA) && is_numeric($operandB)) {
+                    switch ($operator) {
+                        case '+':
+                            $result = $operandA + $operandB;
+                            break;
+                        case '-':
+                            $result = $operandA - $operandB;
+                            break;
+                        case '*':
+                            $result = $operandA * $operandB;
+                            break;
+                        case '/':
+                            $result = $operandA / $operandB;
+                            break;
+                        default:
+                            $result = "Ошибка, программа работает только с этими операторами: +, -, *, /";
+                            break;
+                    }
+                    $result = "Результат: " . $result . "\r\n";
+                } else {
+                    $result = "Результат: Ошибка, 1 и 3 параметры должны быть числом\r\n";
+                }
+            } else {
+                $result = "Результат: Ошибка, метод ожидает 3 параметра\r\n";
             }
-            $result = "Результат: " . $result . "\r\n";
             print($result);
         }
     }
