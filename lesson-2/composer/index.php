8ad0f605c4ae6527fcdfb5354e6ec0bb881383b0
neomulik <neomulik@hotmail.com>
2014-10-21 15:46:37 +0300

    Add some error handling

--- lesson-2/composer/index.php
+++ lesson-2/composer/index.php
@@ -5,14 +5,18 @@ require 'vendor/autoload.php';
 use Elibyy\Reader;
 
 if ($argv[1] !== null) {
-    $reader = new Reader($argv[1]);
-    $status = $reader->unzip();
-    if ($status !== false) {
-        $result = "Archive unpacked successfully\r\n";
+    if (file_exists($argv[1])) {
+        $reader = new Reader($argv[1]);
+        $status = $reader->unzip();
+        if ($status !== false) {
+            $result = "Archive unpacked successfully!\r\n";
+        } else {
+            $result = "Error: Archive could not unpack.\r\n";
+        }
     } else {
-        $result = "Archive could not unpack\r\n";
+        $result = "Error: This file ". $argv[1] ." is doesn't exist!\r\n";
     }
 } else {
-    $result = "Insert file name\r\n";
+    $result = "Error: Insert file name.\r\n";
 }
 print($result);
