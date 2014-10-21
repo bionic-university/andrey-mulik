376d85cf8c1509c1b2f5f070f279b691b814942a
neomulik <neomulik@hotmail.com>
2014-10-21 16:12:17 +0300

    change some strings to PSR standart

--- lesson-2/hw/index.php
+++ lesson-2/hw/index.php
@@ -1,12 +1,14 @@
 <?php
 error_reporting(0);
 
-    class calculator {
+    class calculator
+    {
         public static $info_1 = "Ошибка, программа работает только с этими операторами: +, -, *, /";
         public static $info_2 = "Результат: Ошибка, 1 и 3 параметры должны быть числом\r\n";
         public static $info_3 = "Результат: Ошибка, метод ожидает 3 параметра (Операнд, Оператор, Операнд)\r\n";
 
-        public static function doIt ($operandA, $operator, $operandB) {
+        public static function doIt ($operandA, $operator, $operandB)
+        {
             if ($operandA !== null && $operandB !== null && $operator !== null) {
                 if (is_numeric($operandA) && is_numeric($operandB)) {
                     switch ($operator) {
