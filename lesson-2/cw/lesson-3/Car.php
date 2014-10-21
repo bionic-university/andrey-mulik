d72dae54f3db117c6a5d11a892783516d9255dbd
neomulik <neomulik@hotmail.com>
2014-10-21 10:18:39 -0700

    classWork

---
+++ lesson-2/cw/lesson-3/Car.php
@@ -0,0 +1,23 @@
+<?php
+
+class Car
+{
+
+    private function rotateKey()
+    {
+        $this->detonateTheFuel();
+        $this->rotateTheEngine();
+
+        return 'Result ok';
+    }
+
+    private function detonateTheFuel()
+    {
+
+    }
+
+    private function rotateTheEngine()
+    {
+
+    }
+}
